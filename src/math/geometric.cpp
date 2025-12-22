dcde855fe0586efdcf6db167b5901a5580bf16fd
Zachery Pipes <164957446+ZachPipes@users.noreply.github.com>
2025-12-21 21:47:16 -0600

    geometric.cpp: Added const to all function parameters

--- src/math/geometric.cpp
+++ src/math/geometric.cpp
@@ -5,7 +5,7 @@
 
 namespace fastdist::math {
 
-    double geometric_pmf_scalar(int k, double p) {
+    double geometric_pmf_scalar(const int k, const double p) {
         // p must be in (0,1]
         if (!std::isfinite(p) || p <= 0.0 || p > 1.0) {
             return std::numeric_limits<double>::quiet_NaN();
@@ -19,7 +19,7 @@ namespace fastdist::math {
         return p * std::pow(1.0 - p, k - 1);
     }
 
-    double geometric_cdf_scalar(int k, double p) {
+    double geometric_cdf_scalar(const int k, const double p) {
         if (!std::isfinite(p) || p <= 0.0 || p > 1.0) {
             return std::numeric_limits<double>::quiet_NaN();
         }
@@ -31,21 +31,21 @@ namespace fastdist::math {
         return 1.0 - std::pow(1.0 - p, k);
     }
 
-    double geometric_mean(double p) {
+    double geometric_mean(const double p) {
         if (!std::isfinite(p) || p <= 0.0 || p > 1.0) {
             return std::numeric_limits<double>::quiet_NaN();
         }
         return 1.0 / p;
     }
 
-    double geometric_variance(double p) {
+    double geometric_variance(const double p) {
         if (!std::isfinite(p) || p <= 0.0 || p > 1.0) {
             return std::numeric_limits<double>::quiet_NaN();
         }
         return (1.0 - p) / (p * p);
     }
 
-    double geometric_stddev(double p) {
+    double geometric_stddev(const double p) {
         if (!std::isfinite(p) || p <= 0.0 || p > 1.0) {
             return std::numeric_limits<double>::quiet_NaN();
         }
